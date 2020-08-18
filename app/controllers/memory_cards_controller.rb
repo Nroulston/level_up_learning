402811ea99fc1a71509fc1b51a1710ddb3fb330a
Nroulston <nicolas.roulston@gmail.com>
2020-08-17 19:46:43 -0700

    refactored memory_card controller,

--- app/controllers/memory_cards_controller.rb
+++ app/controllers/memory_cards_controller.rb
@@ -7,20 +7,35 @@ class MemoryCardsController < ApplicationController
   
   def new
     if params[:memory_deck_id]
-        @deck = MemoryDeck.find(params[:memory_deck_id])
-        @card = MemoryCard.new
-   
+       set_memory_deck
+        new_memory_card
+  
     else
-        @card = MemoryCard.new
+        new_memory_card
     end
   end
   
   def create
     raise params.inspect
+    if params[:memory_deck_id]
+      set_memory_deck
+    
+    end
   end
 
 
   private
+  def new_memory_card
+    @card = MemoryCard.new
+  end
+
+  def set_memory_deck
+    @deck = MemoryDeck.find(params[:memory_deck_id])
+  end
+
+  def memory_card_params
+    params.require(:memory_card).permit(:question, :answer)
+  end
   def set_memory_card
     @memory_card = MemoryCard.find(params[:id])
   end
