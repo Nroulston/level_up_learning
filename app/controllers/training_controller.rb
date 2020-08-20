f2ac7c6c4204f2125ebf9aa2eb62a8536a602ad2
Nroulston <nicolas.roulston@gmail.com>
2020-08-19 17:57:47 -0700

    added to flow of cards training

--- app/controllers/training_controller.rb
+++ app/controllers/training_controller.rb
@@ -2,29 +2,34 @@ class TrainingController < ApplicationController
   
   def index  
 
-   @cards = cards_needing_practiced 
+    cards_needing_practiced 
+     
     
   end
  
   def new
     card_to_practice
-    
   end
 
   def show
-
+    card_to_practice
   end
 
-  
+  def update
+    if params[:advance_card]
+      
+    end
+  end
   private
   
   def cards_needing_practiced
     current_user.cards_needing_practice ||= current_user.memory_cards.joins(:deck_card_records).where("deck_card_records.practice_date <= 'Time.now'")
+    current_user.save
   end
   
-  def first_card_to_practice
-    @card = cards_needing_practiced
+  def card_to_practice
+    if cards_needing_practiced
+      @card ||= current_user.cards_needing_practice.first
+    end
   end
-  
-  
 end
