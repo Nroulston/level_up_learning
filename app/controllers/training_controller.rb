53b0ac87882adac14e91b54f531e77a4e35645d8
Nroulston <nicolas.roulston@gmail.com>
2020-08-20 09:07:37 -0700

    moved logic from training controller to training model, also established class constant to house interval steps for training.

--- app/controllers/training_controller.rb
+++ app/controllers/training_controller.rb
@@ -1,6 +1,7 @@
 class TrainingController < ApplicationController
   
   def index  
+    
     cards_needing_practiced 
   end
  
@@ -16,16 +17,4 @@ class TrainingController < ApplicationController
     if params[:advance_card]
     end
   end
-  private
-  
-  # def cards_needing_practiced
-  #   current_user.cards_needing_practice ||= current_user.memory_cards.joins(:deck_card_records).where("deck_card_records.practice_date <= 'Time.now'")
-  #   current_user.save
-  # end
-  
-  # def card_to_practice
-  #   if cards_needing_practiced
-  #     @card ||= current_user.cards_needing_practice.first
-  #   end
-  # end
 end
