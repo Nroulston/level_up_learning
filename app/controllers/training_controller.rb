de5930853053f3a2ca394d7e66880e5e87314a98
Nroulston <nicolas.roulston@gmail.com>
2020-08-20 19:41:21 -0700

    refactored logic from training controller to training model

--- app/controllers/training_controller.rb
+++ app/controllers/training_controller.rb
@@ -14,28 +14,11 @@ class TrainingController < ApplicationController
   end
 
   def update
-    current_record = DeckCardRecord.record_of_users_memory_card(@card)
-
-    if params[:advance_card]
-
-     current_record.practice_interval_counter = Training.practice_interval_steps[Training.practice_interval_steps.index(current_record.practice_interval_counter) + 1]
-
-     current_record.practice_date = Time.now.next_day(current_record.practice_interval_counter)
-
-     current_user.cards_needing_practice.delete_at(0)
-
-     if current_user.cards_needing_practice.blank?
-      current_user.cards_needing_practice = nil
-     end
-
-     
-    else
-      current_record.practice_interval_counter = Training.practice_interval_steps[0]
-      
-      current_user.cards_needing_practice << current_user.cards_needing_practice.shift
-    end
-    current_user.save
-    current_record.save
+    @current_record = DeckCardRecord.record_of_users_memory_card(@card)
+    
+    Training.advance_card_in_interval_set_or_reset_to_zero(params, @current_record, current_user)
+    
+    
     if current_user.cards_needing_practice
       redirect_to new_training_path
     else
