9cf36d691b147c5151b8f32574d4cff64e088099
Nroulston <nicolas.roulston@gmail.com>
2021-01-06 20:40:39 -0700

    add test for validity

--- spec/models/training_spec.rb
+++ spec/models/training_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Training, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :trainings
+  
+
+  it 'is valid when created' do
+    expect(Training.new).to be_valid
+  end
+
+
+  describe '#practice_interval_steps' do
+    it 'returns practice interval steps' do
+        pending('This should not run')
+        expect(training.practice_interval_steps()).to eql[0,1,7,16,35,70]
+    end
+  end
+
 end
